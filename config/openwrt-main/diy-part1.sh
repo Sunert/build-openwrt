019f2b39af236c82e3a45f790295091a1dba6647
Best <47924389+Sunert@users.noreply.github.com>
2024-01-31 14:19:21 +0800

    Update diy-part1.sh

--- config/openwrt-main/diy-part1.sh
+++ config/openwrt-main/diy-part1.sh
@@ -8,7 +8,9 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
+sed -i '$a src-git packages https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '$a src-git appfilter https://github.com/OpenWrt-Actions/OpenAppFilter' feeds.conf.default
 # other
 # rm -rf package/utils/{ucode,fbtest}
 
